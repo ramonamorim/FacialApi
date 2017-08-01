e9c23105466223ad22d417dad613768a666772a2
ramonamorim <amorimramon@outlook.com>
2017-07-31 22:18:29 -0300

    initial commit

---
+++ src/main/java/br/com/facial/facial/FacialApplication.java
@@ -0,0 +1,12 @@
+package br.com.facial.facial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacialApplication.class, args);
+	}
+}
